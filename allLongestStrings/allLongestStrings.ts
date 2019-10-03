74a45fe4bba0c9b6f6d1bd73458fbc90a914301f
Michael Bergen <webtechmike@gmail.com>
2019-10-02 20:45:12 -0700

    allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,21 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  // keep track of index of longest string
+  // iterate input if next string is longer, then replace longestStringIndex
+  // filter input by strings with length matching longest string
 
+  let longestIndex = 0;
+
+  for (let i = 0; i < inputArray.length; i++) {
+    if(inputArray[i].length > inputArray[longestIndex].length) {
+      longestIndex = i;
+    }
+  }
+
+  return inputArray.filter((str) => str.length === inputArray[longestIndex].length);
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["affdba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["affdba", "aa", "ad", "vcd", "abdfaaa"]));
+console.log(allLongestStrings(["aba", "afdaa", "affffd", "vfffcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa", "ad", "vcdd", "aadba"]));
