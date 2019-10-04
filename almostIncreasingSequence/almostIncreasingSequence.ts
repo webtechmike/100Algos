a80d76af0e5a5c5b452b09bc7be4da0d3be90aea
Michael Bergen <webtechmike@gmail.com>
2019-10-03 23:39:16 -0700

    update almostIncreasingSequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,23 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+  // iterate the sequence 
+  // if any number is less than the previous number set count it
+  // if it happens again count it again and return false
 
+  let count = 0;
+
+  for(let i = 0; i < sequence.length; i++) {
+    if(sequence[i] <= sequence[i - 1]) {
+      count += 1;
+      if(sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+        return false;
+      }
+    }
+  }
+
+  return count <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 4])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 4, 5, 2, 1])) 
