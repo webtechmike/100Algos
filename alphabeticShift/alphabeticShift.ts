cbfd584248394b78c5ebadac92ea89727f520b45
Michael Bergen <webtechmike@gmail.com>
2019-10-04 22:36:29 -0700

    add alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,25 @@
 function alphabeticShift(inputString: string): string {
+  // store alphabet in a string
+  // initialize an empty string to store result of shifting letters
+  // iterate inputString and append output string with next value in alphabet string
+  // return output string
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let output = '';
 
+  inputString.split('').forEach((letter, index) => {
+    let location = alphabet.indexOf(letter);
+    if (location === 25) {
+      output = output.concat(alphabet[0]);
+    } else {
+      output = output.concat(alphabet[location+1]);
+    }
+
+    console.log(alphabet[location])
+    console.log(output)
+
+  });
+
+  return output;
 }
 
 console.log(alphabeticShift('crazy'));
