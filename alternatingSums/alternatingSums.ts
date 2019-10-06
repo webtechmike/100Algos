4b7724fecc363b0eb7c6ed560a46828add7198ca
Michael Bergen <webtechmike@gmail.com>
2019-10-06 10:43:24 -0700

    add alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,22 @@
 function alternatingSums(a: number[]): number[] {
+  let teamA = [];
+  let teamB = [];
+  let result = [];
 
+  a.map((t, i) => {
+    if(i % 2 === 0) {
+      teamA.push(t)
+    } else {
+      teamB.push(t)
+    }
+  });
+
+  console.log(teamA, teamB);
+
+  result.push(teamA.reduce((acc, current) => acc+current))
+  result.push(teamB.reduce((acc, current) => acc+current))
+
+  return result;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
