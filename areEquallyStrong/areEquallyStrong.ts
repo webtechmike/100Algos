8393da8b1a188638cea28ffaade44dd6449d16dd
Michael Bergen <webtechmike@gmail.com>
2019-10-06 21:37:55 -0700

    add equallyStrong check

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,10 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+  const yourMaxStrength = yourLeft + yourRight;
+  const friendsMaxStrength = friendsLeft + friendsRight;
 
+  return yourMaxStrength > friendsMaxStrength || friendsMaxStrength > yourMaxStrength ? false : true;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
